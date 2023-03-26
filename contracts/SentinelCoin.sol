8e0a5c4dbad932a834192a389cdb823bf748b3b1
carlos calderon <cdcalderon@gmail.com>
2023-03-25 21:40:19 -0500

    add AddressBanned event and add access controls

--- contracts/SentinelCoin.sol
+++ contracts/SentinelCoin.sol
@@ -7,21 +7,29 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract SentinelCoin is ERC20, Ownable {
     mapping(address => bool) private bannedAddresses;
 
+    event AddressBanned(address indexed _address, bool _banned);
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
     function banAddress(address _address) public onlyOwner {
         bannedAddresses[_address] = true;
+        emit AddressBanned(_address, true);
     }
 
     function unbanAddress(address _address) public onlyOwner {
         bannedAddresses[_address] = false;
+        emit AddressBanned(_address, false);
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {    
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(!bannedAddresses[msg.sender], "Sender is banned from sending tokens");
+        require(!bannedAddresses[recipient], "Recipient is banned from receiving tokens");
         return super.transfer(recipient, amount);
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(!bannedAddresses[sender], "Sender is banned from sending tokens");
+        require(!bannedAddresses[recipient], "Recipient is banned from receiving tokens");
         return super.transferFrom(sender, recipient, amount);
     }
 }
