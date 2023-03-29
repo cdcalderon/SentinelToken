363d868fe68926d7e6ecad636a724154e11662aa
carlos calderon <cdcalderon@gmail.com>
2023-03-29 12:45:05 -0500

    avoid compile warnings

--- contracts/BanToken.sol
+++ contracts/BanToken.sol
@@ -1,6 +1,6 @@
-// // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-// pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
 // import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 // import "@openzeppelin/contracts/access/Ownable.sol";
